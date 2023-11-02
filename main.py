22985addfcedb4e21c14ea0c210416226a2122e8
Jack Draper <drapj002@gmail.com>
2023-11-02 23:09:21 +0000

    chore: initial code & workflow

---
+++ main.py
@@ -0,0 +1,14 @@
+from dislord import interaction, server, serverless
+
+
+@interaction.command(name="hello")
+def hello():
+    return "hello world"
+
+
+def serverless_handler(*args, **kwargs):
+    serverless.serverless_handler(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    server.start_server()
