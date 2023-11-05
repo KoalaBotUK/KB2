a3ddfefa7d605cb7de320328fea6e812a48b2683
Jack Draper <drapj002@gmail.com>
2023-11-05 20:27:37 +0000

    chore: update to use command options

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
-from dislord import interaction, server, serverless
+import os
+import random
+import string
+
+import dislord
+from dislord.models.command import ApplicationCommandOption, ApplicationCommandOptionType
+from dislord.models.interaction import Interaction, InteractionResponse
+
+PUBLIC_KEY = os.environ.get("PUBLIC_KEY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+client = dislord.ApplicationClient(PUBLIC_KEY, BOT_TOKEN)
 
 
-@interaction.command(name="hello")
-def hello():
-    return "hello world!"
+@client.command(name="verify", description="Verify your email with Koala",
+                options=[
+                    ApplicationCommandOption.from_kwargs(name="email", description="Your email to be verified",
+                                                         type=ApplicationCommandOptionType.STRING, required=True,
+                                                         client=client)])
+def verify(interaction: Interaction, email: str):
+    token = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+    print(token)
+    # db.save(email, token)
+    return InteractionResponse.message(content="Please verify yourself using the command you have been emailed")
 
 
-def serverless_handler(*args, **kwargs):
-    return serverless.serverless_handler(*args, **kwargs)
+@client.command(name="confirm", description="Send token to complete email verification",
+                options=[
+                    ApplicationCommandOption.from_kwargs(name="token", description="Token you have been emailed",
+                                                         type=ApplicationCommandOptionType.STRING, required=True,
+                                                         client=client)])
+def confirm(interaction: Interaction, token: str):
+    token = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+    print(token)
+    success = True
+    # success = db.check(email, token)
+    if success:
+        content = "Verification Successful"
+    else:
+        content = "Could not verify with token provided"
+    return InteractionResponse.message(content=content)
 
 
-if __name__ == '__main__':
-    server.start_server()
+def serverless_handler(event, context):  # Not needed if using server
+    client.serverless_handler(event, context)
+
+
+if __name__ == '__main__':  # Not needed if using serverless
+    client.sync_commands()
+    dislord.server.start_server(client, host='0.0.0.0', debug=True, port=8123)
