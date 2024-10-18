e3cd60e88ca7325cc24ab94fe4da5a75de0261ba
Jack Draper <drapj002@gmail.com>
2024-10-19 00:27:03 +0100

    chore: move sync to api

--- kb2/ext/koala/api.py
+++ kb2/ext/koala/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from kb2.api import app
+from kb2.client import client
 from kb2.ext.koala import mappers
 from kb2.jwt import auth
 from kb2.ext.koala.models import Guild
@@ -11,5 +12,12 @@ router = APIRouter(prefix="/koala")
 @router.get("/guild/{guild_id}", dependencies=[Depends(auth.is_owner)])
 async def get_guild(guild_id: str):
     return mappers.guild_to_dto(Guild.get(guild_id))
+
+
+@router.post("/sync", dependencies=[Depends(auth.is_owner)])
+async def sync():
+    client.sync_commands()
+    client.sync_commands(guild_ids=[g.id for g in client.guilds])
+    return {"message": "Synced commands for all guilds"}
 
 app.include_router(router)
