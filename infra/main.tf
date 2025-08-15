d3003a528f8b642ee7c8609dada1b2850b3b15f7
Jack Draper <drapj002@gmail.com>
2025-08-15 16:49:43 -0400

    chore: ui deployment

--- infra/main.tf
+++ infra/main.tf
@@ -31,12 +31,18 @@ module "lambda" {
   discord_bot_token = var.discord_bot_token
 }
 
+module "s3" {
+  source = "./modules/data/s3"
+  deployment_env = var.deployment_env
+}
+
 module "api" {
   source = "./modules/api/api-gateway"
   deployment_env = var.deployment_env
   root_domain_name = var.root_domain_name
   lambda_function_invoke_arn = module.lambda.lambda_function_invoke_arn
   lambda_function_name = module.lambda.lambda_function_name
+  ui_hostname = module.s3.ui_hostname
 }
 
 module "dynamodb" {
