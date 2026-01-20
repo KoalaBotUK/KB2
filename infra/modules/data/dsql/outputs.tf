197ba9de3183e66f5944e06df7aa6c0b37044db2
Jack Draper <drapj002@gmail.com>
2026-01-19 22:56:08 -0500

    fix: cluster identifier tf

--- infra/modules/data/dsql/outputs.tf
+++ infra/modules/data/dsql/outputs.tf
@@ -1,5 +1,5 @@
 data "aws_region" "current" {}
 
 output "dsql_endpoint" {
-  value = "${aws_dsql_cluster.default.id}.dsql.${data.aws_region.current.region}.on.aws"
+  value = "${aws_dsql_cluster.default.identifier}.dsql.${data.aws_region.current.region}.on.aws"
 }
